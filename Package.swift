8f3fe14f05711b42a5721997ec90f3a67be2ef5c
Norio Nomura <norio.nomura@gmail.com>
2017-02-22 16:34:00 +0900

    Use product API in Swift 3 if `PRODUCE_DYLIB` environment variable defined

--- Package.swift
+++ Package.swift
@@ -1,5 +1,18 @@
+import Foundation
 import PackageDescription
 
 let package = Package(
     name: "DLSQLite"
 )
+
+#if !swift(>=4)
+if let _ = ProcessInfo.processInfo.environment.index(forKey: "PRODUCE_DYLIB") {
+    products.append(
+        Product(
+            name: "DLSQLite",
+            type: .Library(.Dynamic),
+            modules: ["DLSQLite"]
+        )
+    )
+}
+#endif
